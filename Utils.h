181db9fe265ef6c82cde60491b08fe4e314fa6d4
zorka5 <zofiakubrak@gmail.com>
2023-09-03 14:37:17 +0200

    add additional library utilities

--- Utils.h
+++ Utils.h
@@ -3,3 +3,9 @@
 #include <string>
 
 std::string read_file_as_string(const char* const& path);
+
+template<class... Ts>
+struct overloaded : Ts... { using Ts::operator()...; };
+
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;

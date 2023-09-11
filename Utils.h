ccca3de80d9a570cc37a0f7bdb640b2c9313967a
zorka5 <zofiakubrak@gmail.com>
2023-09-11 14:27:40 +0200

    removed unused code

--- Utils.h
+++ Utils.h
@@ -3,9 +3,3 @@
 #include <string>
 
 std::string read_file_as_string(const char* const& path);
-
-template<class... Ts>
-struct overloaded : Ts... { using Ts::operator()...; };
-
-template<class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
